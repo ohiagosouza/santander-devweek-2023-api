719d959ba134da292151b30456de518473b79508
ohiagosouza <ohiagosouza@icloud.com>
2023-07-27 20:10:51 -0300

    Implementado a camada de domínio

---
+++ src/main/java/com/hiagosouza/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package com.hiagosouza.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/com/hiagosouza/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hiagosouza.domain.repository;
+
+
+import com.hiagosouza.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
