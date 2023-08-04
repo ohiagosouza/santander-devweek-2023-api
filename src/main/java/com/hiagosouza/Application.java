68ec61309fc13f91d0a14cd0e45ee2a9c3f24790
ohiagosouza <ohiagosouza@icloud.com>
2023-08-03 21:55:38 -0300

    CORS update #2

--- src/main/java/com/hiagosouza/Application.java
+++ src/main/java/com/hiagosouza/Application.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL.")})
+@OpenAPIDefinition(servers = { @Server(url = "/*", description = "Default Server URL.")})
 @SpringBootApplication
 public class Application {
 
@@ -14,3 +17,4 @@ public class Application {
 	}
 
 }
+
